a67b2ef5258b0591af0459c4dad6a6c59fa61ea1
trackpadpro <valentinsrichter@gmail.com>
2022-12-17 23:17:17 -0500

    add basic impedance caluclations

--- lib/circuit/circuit.cpp
+++ lib/circuit/circuit.cpp
@@ -1,1 +1,24 @@
 #include "circuit.h"
+
+bool Impedance::simplify(double freq){
+    if(R!=0){
+        real = R;
+    }
+    else if(L!=0){
+        imag = freq*L;
+    }
+    else if(C!=0){
+        imag = -1/(freq*C);
+    }
+
+    mag = sqrt(real*real+imag*imag);
+    phase = atan(imag/real);
+}
+
+bool Circuit::simplify(double freq){
+    for(auto& series: components){
+        for(auto& parallel: series){
+            parallel->simplify(freq);
+        }
+    }
+}
--- lib/circuit/circuit.h
+++ lib/circuit/circuit.h
@@ -1,6 +1,23 @@
 #if !defined(CIRCUIT_H)
 #define CIRCUIT_H
 
+#include <fstream>
+#include <vector>
+#include <cmath>
 
+class Impedance{
+    public:
+        bool simplify(double freq); //freq in rad/s
+    protected:
+        double real, imag, R, L, C, mag, phase;
+};
+
+class Circuit: public Impedance{
+    public:
+        friend std::fstream& operator>>(std::fstream& os,Circuit& input);
+        friend std::fstream& operator<<(std::fstream& os,const Circuit& output);
+    private:
+        std::vector<std::vector<Impedance*>> components;
+};
 
 #endif
